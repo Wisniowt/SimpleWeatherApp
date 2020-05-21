55431f37273ffe5cf2c0de8f86c9b7f6aa1682cc
Tom <wisniowt@tcd.ie>
2020-05-21 22:47:27 +0100

    Added style. Fixed errors.

---
+++ weatherAppServer.js
@@ -0,0 +1,46 @@
+"use strict";
+
+// import * as axios from 'axios';
+
+const axios = require('axios')
+const express = require('express')
+const app = express()
+const port = 3000
+
+const path=require("path")
+let publicPath = path.resolve(__dirname,"public")
+app.use(express.static(publicPath))
+
+app.get('/:city', sendForecast)
+
+
+app.listen(port, ()=>console.log(`Example app listening on port ${port}!`))
+
+
+function sendForecast(req, res){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    let city= req.params.city
+
+    
+
+    let url = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=metric" + "&APPID=3e2d927d4f28b456c6bc662f34350957";
+    console.log(url)
+    axios
+      .get(url)
+      .then(response => {
+        console.log(response.data);
+
+        
+        res.json({ result: response.data});
+
+         
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
+}
+
+
+
